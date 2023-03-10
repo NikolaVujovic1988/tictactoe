dfe00d8af7f677f1dc33b1808e18688eb12b4411
Nikola Vujovic <nikola.vujovic1988@yahoo.com>
2023-03-10 23:19:01 +0100

    show  x and o

--- script.js
+++ script.js
@@ -10,5 +10,16 @@ function fillShape(id) {
     }
 
     fields[id] = currentShape;
-    console.log(fields);
+    draw();
+}
+
+function draw() {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'cross') {
+            document.getElementById('cross-' + i).classList.remove('d-none');
+        }
+        if (fields[i] == 'circle') {
+            document.getElementById('circle-' + i).classList.remove('d-none');
+        }
+    }
 }
